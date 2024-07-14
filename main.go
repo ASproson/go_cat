7d89edc0464f6c314edcc29f66877e14f7b0cf5b
ASproson <a.sproson@outlook.com>
2024-07-14 08:44:19 +0100

    Refactors

--- main.go
+++ main.go
@@ -6,24 +6,8 @@ import (
 	"os"
 )
 
-func main(){
-	// data, err := os.ReadFile("test.txt")
-	// if(err != nil){
-	// 	fmt.Println(err)
-	// } else {
-	// 	fmt.Println(string(data))
-	// }
-	if(len(os.Args) != 4){
-		fmt.Println("Usage: go run main.go head -n1 [file]")
-	}
-
-	if os.Args[2] != "-n1" {
-		fmt.Println("Usage: go run main.go head -n1 [file]")
-	}
-
-	fileName := os.Args[3]
-	fmt.Println(fileName)
-
+// go run main.go head -n1 test.txt
+func printFirstLine(fileName string){
 	file, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
@@ -41,8 +25,20 @@ func main(){
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Scanner error:", err)
 	}
-
-
 }
 
-// go run main.go head -n1 test.txt 
+func main(){
+	if(len(os.Args) != 4){
+		fmt.Println("Usage: go run main.go head -n1 [file]")
+	}
+	
+	if os.Args[2] != "-n1" {
+		fmt.Println("Usage: go run main.go head -n1 [file]")
+	}
+	
+	fileName := os.Args[3]
+	if os.Args[1] == "head" && os.Args[2] == "-n1" {
+		printFirstLine(fileName)
+	}
+
+}
